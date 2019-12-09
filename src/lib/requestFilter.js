1a012947a90577db61569c83339d9bd5b3f4a251
Carl Wirth <cdwirth1@gmail.com>
2019-12-09 17:52:48 -0500

    request filter - discussion and set up

---
+++ src/lib/requestFilter.js
@@ -0,0 +1,9 @@
+const request = data => {
+    // check if data is a number of an object
+    // and make a rerquest depeding on what kind of variable
+    // is it
+};
+
+const makeRequest = url => {
+
+};
