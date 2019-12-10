945fa570423f5671a65a075193cc7943524d7ebd
Carl Wirth <cdwirth1@gmail.com>
2019-12-09 19:42:15 -0500

    fetch request build

--- src/lib/requestFilter.js
+++ src/lib/requestFilter.js
@@ -1,9 +1,28 @@
+const DR = require("./dataRefactoring");
+
+
 const request = data => {
+
+    if (typeOf(data) === "number") {
+        //construct url
+        const url = `https://pokeapi.co/api/v2/pokemon/${data}`;
+        const x = makeRequest(url);
+        return x;
+
+    } else if (typeOf(data) === "object") {
+        // construct url
+        const url = `https://pokeapi.co/api/v2/pokemon/${data.id}`;
+        const x = makeRequest(url);
+        return x;
+    };
+
     // check if data is a number of an object
     // and make a rerquest depeding on what kind of variable
     // is it
 };
 
-const makeRequest = url => {
-
+const makeRequest = (url) => {
+    fetch(url).then(res => res.json()).then(res => {
+        return DR.minify(res);
+    });
 };
